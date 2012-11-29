c1e300b64919aecc20e3631750075a7022281031
Jerry D'Antonio <stumpjumper@gmail.com>
2012-11-28 22:13:01 -0500

    Removed unnecessary gems and associated files and directories.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,10 +5,6 @@ require 'bundler/setup'
 require 'ini_file'
 
 require 'rspec'
-require 'factory_girl'
-
-FactoryGirl.definition_file_paths << File.join(File.dirname(__FILE__), '../factories')
-FactoryGirl.find_definitions
 
 RSpec.configure do |config|
 
