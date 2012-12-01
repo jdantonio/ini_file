2b32a29f24a14386f96738e49bfe0f411dace218
Jerry D'Antonio <jdantonio@virtualhold.com>
2012-12-01 08:08:31 -0500

    Implememted all red specs for IniFile.load method.

--- spec/ini_file_spec.rb
+++ spec/ini_file_spec.rb
@@ -7,21 +7,36 @@ describe IniFile do
   subject { IniFile }
 
   let(:filename) { 'test.ini' }
+  let(:contents) { '; this is a comment' }
 
   context '.load' do
 
     it 'throws an exception if the file does not exist' do
-      pending
       lambda { subject.load(filename) }.should raise_error
     end
 
-    it 'throws an exception on inadequate file permissions'
+    it 'throws an exception on inadequate file permissions' do
+      FileUtils.touch(filename)
+      FileUtils.chmod(0000, filename)
+      lambda { subject.load(filename) }.should raise_error
+    end
 
-    it 'passes the file contents to a new Contents object'
+    it 'passes the file contents to a new Contents object' do
+      File.open(filename, 'w') {|f| f.write(contents) }
+      IniFile::Contents.should_receive(:new).with(contents)
+      subject.load(filename)
+    end
 
-    it 'bubbles any exceptions thrown by the Contents constructor'
+    it 'bubbles any exceptions thrown by the Contents constructor' do
+      FileUtils.touch(filename)
+      IniFile::Contents.stub(:new).with(any_args()).and_raise(StandardError)
+      lambda { subject.load(filename) }.should raise_error
+    end
 
-    it 'returns the new contents object'
+    it 'returns the new contents object' do
+      File.open(filename, 'w') {|f| f.write(contents) }
+      IniFile.load(filename).should be_instance_of IniFile::Contents
+    end
 
   end
 
