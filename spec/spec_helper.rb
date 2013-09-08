82d1d08d1329a13da08430d50c71f23064c3bb29
Jerry D'Antonio <stumpjumper@gmail.com>
2013-09-07 21:56:11 -0400

    Added SimpleCov and Coveralls.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,19 @@
 $:.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
 
+require 'simplecov'
 require 'coveralls'
-Coveralls.wear!
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+
+SimpleCov.start do
+  project_name 'ini_file'
+  add_filter '/doc/'
+  add_filter '/pkg/'
+  add_filter '/spec/'
+end
 
 require 'bundler/setup'
 
@@ -12,4 +24,11 @@ require 'fakefs'
 
 RSpec.configure do |config|
 
+  config.before(:suite) do
+    FakeFS.activate!
+  end
+
+  config.after(:suite) do
+    FakeFS.deactivate!
+  end
 end
